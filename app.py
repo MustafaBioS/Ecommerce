f5141f0aec7dd788d37d7e8cca3d0a6e9020c552
MustafaBioS <mustaafahany@gmail.com>
2025-12-03 20:40:16 +0200

    ADD - Refund modal and semi responsiveness for it

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
@@ -26,12 +26,34 @@ class Items(db.Model):
     sec_pic = db.Column(db.String(128), nullable=False)
     third_pic = db.Column(db.String(128), nullable=False)
 
+class Refunds(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order = db.Column(db.Integer, nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+
 
 # Routes
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/all')
+def all():
+    return render_template('all.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/refund')
+def refund():
+    order = request.form.get('orderNum')
+    phone = request.form.get('phoneNum')
+    new_request = Refunds(order=order, phone=phone)
+    db.session.add(new_request)
+    db.session.commit()
+    return redirect(url_for("all"))
 
 # Run
 
